5b2ac465111e49da75a910caba1c2d587946f68d
haro99 <45450954+haro99@users.noreply.github.com>
2021-06-01 04:06:59 +0900

    Add files via upload

---
+++ NewBehaviourScript2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using MySql.Data.MySqlClient;
+public class NewBehaviourScript2 : MonoBehaviour
+{
+    private void Start()
+    {
+        MySqlConnection con = new MySqlConnection("server=MYSQLDB; uid=root;pwd=password;database=tekitoo");
+
+        try
+        {
+            con.Open();
+            string sql = "Select * from tableName";
+            MySqlCommand com = new MySqlCommand(sql, con);
+            MySqlDataReader reader = com.ExecuteReader();
+            while (reader.Read())
+            {
+                Debug.Log(reader[0]);
+            }
+            reader.Close();
+        }
+        catch (MySqlException e)
+        {
+            //Debug.Log(e.ToString());
+            Debug.Log("データを取得できませんでした");
+        }
+
+        Debug.Log("");
+    }
+}
